f22e3598f69ccb9a1a9e68429b5219a3856eaf2f
Mbirepinte <mickael.birepinte@gmail.com>
2022-12-09 16:53:42 +0100

    test wireframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React, { Suspense, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { Cylinder, OrbitControls } from '@react-three/drei';
 import './App.css'
-import { Model } from './Model';
+import { Model } from './components/Model';
+import Cylinder3d from './components/Cylinder3d';
+
 
 function App() {
   const [count, setCount] = useState(0)
+   const[wire, setWire] = useState(false)
 
   return (
    <div>
-      <button> Test</button>
+      <button onClick={()=>setWire(!wire)}> Test</button>
       <div className="avatar">
          <Canvas
          camera={{ position: [2, 0, 12.25], fov: 15 }}
@@ -24,6 +27,8 @@ function App() {
          <directionalLight intensity={0.4} />
          <Suspense fallback={null}>
          <Model position={[0.025, -0.9, 0]} />
+         <Cylinder3d position={[1,2,1]} wireframe={wire}/>
+         <Cylinder3d position={[3,1,0]} wireframe={!wire}/>
          </Suspense>
          <OrbitControls />
       </Canvas>
